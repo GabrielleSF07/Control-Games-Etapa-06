b50c0e95ed6d308556d0a9d5e7e51d481987313b
Gabrielle Souza Felix <gabisf011225@gmail.com>
2026-01-10 14:20:03 -0300

    Criação do projeto

---
+++ src/main/java/controlgames/controlgamesetapa06/ControlGamesEtapa06.java
@@ -0,0 +1,9 @@
+
+package controlgames.controlgamesetapa06;
+
+public class ControlGamesEtapa06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
